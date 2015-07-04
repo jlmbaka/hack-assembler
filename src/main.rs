72d82e6ca1b6599b5c28117eb2789a52ec33298e
Jean-Louis Mbaka <jeanlouis.mbaka@gmail.com>
2015-07-04 19:59:13 +0100

    Parser struct unique field is the asm file stream, ready to be parsed.

--- src/main.rs
+++ src/main.rs
@@ -5,48 +5,20 @@ use std::io::prelude::*;
 use std::env;
 
 
-// OWNERSHIP: we cannot use a binding after we’ve moved it aka taken a reference to it or passed it as an args
-// 
-//
-
-
-// enum CommandType {
-// 	A_COMMAND,
-// 	C_COMMAND,
-// 	L_COMMAND,
-// }
-struct Parser<'a> {
-	commands: Vec<&'a str>,
+struct Parser {
+	input_file: File,
 }
 
-impl<'a> Parser<'a> {
-	// remove all white space and comments
-	fn new(asm_filename: &str) -> Parser {
-		let asm_file_content = load_asm(asm_filename);
-		Parser {
-			commands: asm_file_content.trim().split('\n').collect(),
-		}
+impl Parser {
+	fn new(filename: &str) -> Parser {
+		let path = Path::new(filename);
+		let file = match File::open(&path) {
+			Err(why) => panic!("couldn't open {}: {}", path.display(), Error::description(&why)),
+			Ok(file) => file,
+		};
+
+		Parser { input_file: file}
 	}
-}
-
-fn load_asm(filename: &str) -> String {
-	//create a path to the desired file
-	let path = Path::new(filename);
-	let display = path.display();
-
-	// open the path in read-only mode
-	let mut file = match File::open(&path) {
-		Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
-		Ok(file) => file,
-	};
-
-	// Read the file content into a string
-	let mut file_content = String::new();
-	match file.read_to_string(&mut file_content) {
-		Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
-		Ok(_) => print!("{} contains:\n{}", display, file_content),
-	}
-	file_content
 }
 
 fn main() {
