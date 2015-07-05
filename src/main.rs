914ba6f351973b4a9511495fdd3d40412ce9eff1
Jean-Louis Mbaka <jeanlouis.mbaka@gmail.com>
2015-07-05 14:30:40 +0100

    Finally got out of borrow checker fight about input_line. Using input_line.next() instead of for loop on input_lines. Works

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::io::Lines;
 
-use CommandType::{A_COMMAND, C_COMMAND, L_COMMAND};
+use CommandType::{ACommand, CCommand, LCommand};
 
 enum CommandType {
-	A_COMMAND,
-	C_COMMAND,
-	L_COMMAND,
+	ACommand,
+	CCommand,
+	LCommand,
 }
 
 struct Parser {
@@ -29,39 +29,46 @@ impl Parser {
 		};
 
 		let lines = BufReader::new(file).lines(); // iterator
-		Parser { input_lines: lines, current_command: String::new()}
+		Parser {
+			input_lines: lines, 
+			current_command: String::new(),
+		}
 	}
 
 	/// Encapsulates has_more_command and avance method from the proposed API.
 	///
 	///		Are there any more commands in the input?
 	///			Reads the next command from the input and makes it the current command
-	fn parse(mut self) {
-		for line in self.input_lines { // has_more_commands + advance
-			// println!("{0}", line.unwrap().trim());
-			self.current_command = line.unwrap().trim().to_string();
-			match self.command_type() {
-				A_COMMAND 	=> {println!("A_COMMAND: {0}", self.current_command);},
-				C_COMMAND 	=> {println!("C_COMMAND: {0}", self.current_command);},
-				L_COMMAND 	=> {println!("L_COMMAND: {0}", self.current_command);},
+	fn parse(&mut self) {
+		loop {
+			match self.input_lines.next() {
+				Some(line) => {
+					self.current_command = line.unwrap().trim().to_string();
+					match self.command_type() {
+						ACommand => {println!("ACommand: {0}", self.current_command);},
+						CCommand => {println!("CCommand: {0}", self.current_command);},
+						LCommand => {println!("LCommand: {0}", self.current_command);},
+					}
+				},
+				None => break,
 			}
 		}
 	}
 
 	/// Returns the type of the current command.
 	///
-	/// * A_COMMAND: For @xxx where xxx is either a symbol or a decimal number
-	/// * C_COMMAND: For dest=comp;jump
-	/// * L_COMMAND: Pseudo-Command. for (xxx) where xxx is a symbol
+	/// * ACommand: For @xxx where xxx is either a symbol or a decimal number
+	/// * CCommand: For dest=comp;jump
+	/// * LCommand: Pseudo-Command. for (xxx) where xxx is a symbol
 	fn command_type(&self) -> CommandType {
-		A_COMMAND
+		ACommand
 	}
 }
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	if args.len() != 2 {
-		println!("Usage: assembler [PATH_TO_ASM_FILE]");
+		println!("HACK Assembler. Translates assembly (mnemonic) into binary machine code.\n\nUsage:\n\tassembler [PATH_TO_ASM_FILE]");
 		return;
 	}
 	let path_to_asm = &args[1];
